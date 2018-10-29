59315cd80bbcd65aa8e64a952c569e1abf33b82b
Viktor Zetterström <vize1500@student.miun.se>
2018-10-29 15:17:47 +0100

    Added Doxygen comments to ScreenPrinter

--- include/ScreenPrinter.h
+++ include/ScreenPrinter.h
@@ -1,11 +1,10 @@
-/*
- * Filename    ScreenPrinter.h
- * Author      Erik Ström
- * Date        October 2017
- * Version     0.1
-*/
-
-
+/**
+ * @file ScreenPrinter.h
+ * @author Erik Ström
+ * @brief Declaration of ScreenPrinter
+ * @version 0.1
+ * @date 2018-10-29
+ */
 
 #ifndef screenPrinterH
 #define screenPrinterH
@@ -14,27 +13,57 @@
 #include "../terminal/terminal.h"
 #include "Cell_Culture/Population.h"
 
-// Responsible for visually representing the simulation world on screen.
+/**
+ * @class ScreenPrinter
+ * @brief Responsible for visually representing the simulation world on screen
+ * @details Singleton class that uses a Terminal object from submodule Terminal to visually represent the simulation.
+ */
 class ScreenPrinter {
 
 private:
+    /**
+     * @brief Terminal object handling screen output
+     */
     Terminal terminal;
 
+    /**
+     * @brief Empty private constructor.
+     */
     ScreenPrinter() {}
 
 public:
+    /**
+     * @brief Returns the ScreenPrinter singleton instance.
+     * 
+     * @return ScreenPrinter& Reference to ScreenPrinter instance.
+     */
     static ScreenPrinter& getInstance() {
         static ScreenPrinter instance;
         return instance;
     }
 
-
+    /**
+     * @brief Prints Population to screen
+     * @todo Change type of WindowHeight and WindowWidth to Dimensions
+     * 
+     * @param population Reference to Population object
+     */
     void printBoard(Population& population);
 
+    /**
+     * @brief Prints the help screen
+     */
     void printHelpScreen();
 
+    /**
+     * @brief Prints std::string supplied as input.
+     * @param message String containting message.
+     */
     void printMessage(string message);
 
+    /**
+     * @brief Clears the screen.
+     */
     void clearScreen();
 };
 
