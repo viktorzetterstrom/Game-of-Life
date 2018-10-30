679a97a0170f1cb5b53b95f7da25a1eec1fca6ad
Viktor Zetterström <vize1500@student.miun.se>
2018-10-30 17:21:21 +0100

    Added Doxygen comments to RuleFactory

--- include/GoL_Rules/RuleFactory.h
+++ include/GoL_Rules/RuleFactory.h
@@ -1,23 +1,46 @@
-/*
- * Filename    RuleFactory.h
- * Author      Erik Ström
- * Date        October 2017
- * Version     0.1
-*/
+/**
+ * @file RuleFactory.h
+ * @author Erik Ström
+ * @brief Definition of RuleFactory, class that handles RulesOfExistence.
+ * @version 0.1
+ * @date 2018-10-30
+ */
 
 #ifndef RULEFACTORY_H
 #define RULEFACTORY_H
 
 #include "GoL_Rules/RuleOfExistence.h"
 
-// Singleton class to handle creation of RulesOfExistence objects.
+/**
+ * @brief Singleton class to handle creation of RulesOfExistence objects.
+ * 
+ */
 class RuleFactory
 {
 private:
+    /**
+     * @brief Empty private constructor
+     */
     RuleFactory() {}
 
 public:
+    /**
+     * @brief Returns the singleton instance of the class.
+     * 
+     * @return RuleFactory& Reference to the static instance.
+     */
     static RuleFactory& getInstance();
+
+    /**
+     * @brief Returns a pointer to a RuleOfExistence based on given cell map 
+     *  and ruleOfExistence.
+     * @todo Add error message and handling if bad rulename is given. Default
+     *  value 'conway' is never used.
+     * 
+     * @param cells Reference to std::map with Cells mapped against Points
+     * @param ruleName std::string with rulename to be used. Standard is "conway"
+     * @return RuleOfExistence* 
+     */
     RuleOfExistence* createAndReturnRule(map<Point, Cell>& cells, string ruleName = "conway");
 };
 
