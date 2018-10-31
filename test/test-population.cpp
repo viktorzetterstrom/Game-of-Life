03729faa8b8cdac298ddb59b7be6e6d2b1521031
Viktor Zetterström <vize1500@student.miun.se>
2018-10-31 18:10:20 +0100

    Small fixes on Cell tests

---
+++ test/test-population.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file test-cell.cpp
+ * @author Viktor Zetterström (vize1500@student.miun.se)
+ * @brief Unit tests for the class Population.
+ * @version 0.1
+ * @date 2018-10-31
+ */
+
+#include <catch.hpp>
+#include "../include/Cell_Culture/Population.h"
+
